91318dec1c9b38d781d247d7b7d0e16a2abb7eee
gorbunovdvuk <gorbunovdvuk@gmail.com>
2025-05-09 11:50:33 +0300

    Add command type implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "cmd_options.h"
 #include "crypto_guard_ctx.h"
 #include <array>
+#include <fstream>
 #include <iostream>
 #include <openssl/evp.h>
 #include <print>
@@ -87,18 +88,25 @@ int main(int argc, char *argv[]) {
 
         using COMMAND_TYPE = CryptoGuard::ProgramOptions::COMMAND_TYPE;
         switch (options.GetCommand()) {
-        case COMMAND_TYPE::ENCRYPT:
+        case COMMAND_TYPE::ENCRYPT: {
+            std::fstream fin(options.GetInputFile(), std::ios::in);
+            std::fstream fout(options.GetOutputFile(), std::ios::out | std::ios::trunc);
+            cryptoCtx.EncryptFile(fin, fout, options.GetPassword());
             std::print("File encoded successfully\n");
             break;
-
-        case COMMAND_TYPE::DECRYPT:
+        }
+        case COMMAND_TYPE::DECRYPT: {
+            std::fstream fin(options.GetInputFile(), std::ios::in);
+            std::fstream fout(options.GetOutputFile(), std::ios::out | std::ios::trunc);
+            cryptoCtx.DecryptFile(fin, fout, options.GetPassword());
             std::print("File decoded successfully\n");
             break;
-
-        case COMMAND_TYPE::CHECKSUM:
-            std::print("Checksum: {}\n", "CHECKSUM_NOT_IMPLEMENTED");
+        }
+        case COMMAND_TYPE::CHECKSUM: {
+            std::fstream fin(options.GetInputFile(), std::ios::in);
+            std::print("Checksum: {}\n", cryptoCtx.CalculateChecksum(fin));
             break;
-
+        }
         default:
             throw std::runtime_error{"Unsupported command"};
         }
