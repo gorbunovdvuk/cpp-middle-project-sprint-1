72e3848dc03ea810e9ebb46cf113b206aa52d4b5
gorbunovdvuk <gorbunovdvuk@gmail.com>
2025-05-15 17:07:16 +0300

    Add logical empty lines

--- src/cmd_options.cpp
+++ src/cmd_options.cpp
@@ -23,10 +23,13 @@ bool ProgramOptions::Parse(int argc, const char *const argv[]) {
     try {
         po::variables_map vm;
         po::store(po::parse_command_line(argc, argv, desc_), vm);
+
         if (vm.contains("help")) {
             return false;
         }
+
         po::notify(vm);
+
         std::string command = vm["command"].as<std::string>();
         std::ranges::transform(command, command.begin(), ::tolower);
         if (commandMapping_.contains(command)) {
@@ -34,10 +37,12 @@ bool ProgramOptions::Parse(int argc, const char *const argv[]) {
         } else {
             throw std::runtime_error("Argument error: Unknown command '" + command + "'");
         }
+
         if (!vm.contains("input")) {
             throw std::runtime_error("Argument error: input option is required");
         }
         inputFile_ = vm["input"].as<std::string>();
+
         if (command_ == COMMAND_TYPE::ENCRYPT || command_ == COMMAND_TYPE::DECRYPT) {
             if (!vm.contains("output")) {
                 throw std::runtime_error("Argument error: output option is required");
